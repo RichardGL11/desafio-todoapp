2e5f1f7513f688cd62081afaa1e759f00b60b732
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-05-02 13:17:17 -0300

    test: making sure that only the todo owner can update the status to confirmed

--- tests/Feature/Todo/MarkTodoAsCompletedTest.php
+++ tests/Feature/Todo/MarkTodoAsCompletedTest.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
+use function PHPUnit\Framework\assertFalse;
 use function PHPUnit\Framework\assertTrue;
 
 class MarkTodoAsCompletedTest extends TestCase
@@ -22,5 +23,18 @@ class MarkTodoAsCompletedTest extends TestCase
        $response->assertRedirect('/home');
        $todo->refresh();
        assertTrue($todo->status == TodoStatusEnum::COMPLETED);
+    }
+
+    #[Test]
+    public function test_only_the_owner_can_mark_as_completed():void
+    {
+       $user =  User::factory()->create();
+       $wrongUser = User::factory()->create();
+       $todo = Todo::factory()->for($user)->createOne();
+       $this->actingAs($wrongUser);
+       $response = $this->from('/home')->get(route('todos.mark.completed',$todo));
+       $response->assertForbidden();
+       $todo->refresh();
+       assertFalse($todo->status == TodoStatusEnum::COMPLETED);
     }
 }
